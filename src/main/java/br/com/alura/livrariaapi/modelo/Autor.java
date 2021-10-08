5cfdbdd740645dd22c52a223c098740c866bba50
“grsn-educa” <grsn.educa@gmail.com>
2021-10-07 21:15:23 -0300

    mapeamento das entidades livro e autor

--- src/main/java/br/com/alura/livrariaapi/modelo/Autor.java
+++ src/main/java/br/com/alura/livrariaapi/modelo/Autor.java
@@ -1,7 +1,11 @@
 package br.com.alura.livrariaapi.modelo;
 
-import java.sql.Connection;
 import java.time.LocalDate;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,8 +17,13 @@ import lombok.ToString;
 @ToString(exclude = {"nascimento","curriculo","email"})
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "autores")
 public class Autor {
-
+    
+        @Id
+        @GeneratedValue(strategy =  GenerationType.IDENTITY)
+        private Long id;
 	private String nome;
 	private String email;
 	private LocalDate nascimento;
--- src/main/java/br/com/alura/livrariaapi/modelo/Livro.java
+++ src/main/java/br/com/alura/livrariaapi/modelo/Livro.java
@@ -1,22 +1,44 @@
 package br.com.alura.livrariaapi.modelo;
 
 import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
+
 
 @Getter
 @Setter
 //@ToString(exclude = {"dataLancamento"})
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "livros")
 public class Livro {
     
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
     private String titulo;
+    @Column(name = "data_nascimento")
     private LocalDate dataLancamento;
-    private int numeroPaginas;
-    private String autor;
+    @Column(name = "numero_paginas")
+    private Integer numeroPaginas;
+
+    @ManyToOne
+    private Autor autor;
+    
+    
+
     
 }
